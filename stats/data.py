0f5ca8607bb0c7dd26e9eeb9a60c39d92025e1c8
dako <dako@vm>
2019-10-01 21:16:53 +0200

    Fix Identifiers regex

--- stats/data.py
+++ stats/data.py
@@ -6,7 +6,9 @@ import pandas as pd
 # logging setup
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s [%(pathname)s:%(lineno)d]')
+log_format = '%(asctime)s - %(name)s - %(levelname)s'
+log_format += ' - %(message)s [%(pathname)s:%(lineno)d]'
+formatter = logging.Formatter()
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 stream_handler.setFormatter(formatter)
@@ -17,32 +19,52 @@ game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 # sort filenames
 game_files.sort()
 
-logger.info('Loaded %s game files.', len(game_files))
+logger.info('Loaded %s game file names.', len(game_files))
 
-names = ['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event']
+column_names = [
+        'type',
+        'multi2',
+        'multi3',
+        'multi4',
+        'multi5',
+        'multi6',
+        'event',
+        ]
 
 game_frames = []
 for game_file in game_files:
     # read csv file
-    game_frame = pd.read_csv(game_file, names=names)
+    game_frame = pd.read_csv(game_file, names=column_names)
     game_frames.append(game_frame)
 
+logger.info('Loaded %s game frames.', len(game_frames))
 # Concatenate DataFrames
 games = pd.concat(game_frames)
+
 # Clean Values
 games.loc[games['multi5'] == '??', 'multi5'] = ''
+
 # Extract Identifiers
-identifiers = games['multi2'].str.extract(r'(.LS(\d{4}\d{5}))')
+identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
 # identifiers has 2 columns now
+# print(identifiers.head())
+
 # Forward Fill Identifiers
 identifiers = identifiers.fillna(method='ffill')
+# print(identifiers.head())
+
 # Rename Columns
 identifiers.columns = ['game_id', 'year']
+# print(identifiers.head())
+
 # Concatenate Identifier Columns
 games = pd.concat([games, identifiers], axis=1, sort=False)
+
 # Fill NaN Values
 games = games.fillna(' ')
+
 # Categorical Event Type
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
+
 # Print DataFrame
 print(games.head())
